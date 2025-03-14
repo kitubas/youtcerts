e9d6a62f0423d3995b51b340cd2c8f10bf08ef46
henrique-leite <henrique.leite@sensedia.com>
2025-03-14 11:10:57 -0300

    first commit

---
+++ src/test/java/com/youtcerts/YoutcertsUsersTrackingApplicationTests.java
@@ -0,0 +1,15 @@
+package com.youtcerts;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Disabled
+@SpringBootTest
+class YoutcertsUsersTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
